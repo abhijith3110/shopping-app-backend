53fc045b1ed7670f4c8ab505d53cb9c6b6fa9800
dosabhijith <dosabhijith@gmail.com>
2024-11-23 11:07:34 +0530

    feat: implement cors

--- app.js
+++ app.js
@@ -4,13 +4,22 @@ import { connectDB } from './configs/dbConfig.js'
 import categoryRouter from './routes/v1/categoryRoute.js'
 import {notFound, errorHandler} from "./middlewares/errorMiddleware.js"
 import productRouter from './routes/v1/productRoute.js'
+import cors from 'cors'
+import path from 'path'
 
 const app = express()
 
 dotenv.config()
+
+const __dirname = path.resolve()
+
 connectDB()
 
 app.use(express.json())
+
+app.use(cors())
+
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 app.use('/api/v1/category', categoryRouter)
 app.use('/api/v1/product', productRouter)
